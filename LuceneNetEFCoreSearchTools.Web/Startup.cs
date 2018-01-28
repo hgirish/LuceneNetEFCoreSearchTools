eab2bf87a614de322a839dd8aadb0e714a1efa39
hgirish <girish@post.com>
2018-01-27 22:59:11 -0800

    safeguard against null entity (due to stale search indexes)

--- LuceneNetEFCoreSearchTools.Web/Startup.cs
+++ LuceneNetEFCoreSearchTools.Web/Startup.cs
@@ -20,8 +20,13 @@ namespace LuceneNetEFCoreSearchTools.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<AppDbContext>(options =>
+            //options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            {
+                options.UseSqlite("Data Source=LuceneSerach.db");
+            });
 
             services.AddMvc();
 
